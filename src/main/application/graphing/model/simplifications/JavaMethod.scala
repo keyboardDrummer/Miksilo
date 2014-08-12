06d46f7bb44a046c90431b363e5e2b8b517a1d99
Remy <Remy.Willems@ortec-finance.com>
2014-08-12 19:24:16 +0200

    Fixed visualization bug

--- src/main/application/graphing/model/simplifications/JavaMethod.scala
+++ src/main/application/graphing/model/simplifications/JavaMethod.scala
@@ -2,12 +2,12 @@ package application.graphing.model.simplifications
 
 import core.transformation.Contract
 import transformations.javac.classes.ClassC
-import transformations.javac.methods.{CallC, ImplicitReturnAtEndOfMethod, ReturnExpressionC, VariableC}
+import transformations.javac.methods.{CallC, ImplicitReturnAtEndOfMethod, VariableC}
 import transformations.javac.statements.AssignmentC
 
 object JavaMethod extends TransformationGroup {
 
-  override def dependencies: Set[Contract] = Set(ImplicitReturnAtEndOfMethod, AssignmentC, ReturnExpressionC, CallC, VariableC)
+  override def dependencies: Set[Contract] = Set(ImplicitReturnAtEndOfMethod, AssignmentC, CallC, VariableC)
 
   override def dependants: Set[Contract] = Set(ClassC)
 }
---
+++ testResources/postFixIncrement.java
@@ -0,0 +1,8 @@
+class PostFixIncrement {
+  public static void main(java.lang.String[] args)
+  {
+    int x = 1;
+    x++;
+    System.out.print(x++);
+  }
+}
