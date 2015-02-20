ca98cab335a59772d51041b7e448b5512e56ec5b
Remy Willems <rgv.willems@gmail.com>
2015-02-20 08:47:37 +0100

    Added testLongWithoutPoptimize tst.

--- src/test/transformations/javac/expressions/TestLong.scala
+++ src/test/transformations/javac/expressions/TestLong.scala
@@ -1,6 +1,9 @@
 package transformations.javac.expressions
 
+import core.transformation.CompilerFromParticles
 import org.junit.Test
+import transformations.bytecode.additions.PoptimizeC
+import transformations.javac.JavaCompiler
 import util.TestUtils
 
 import scala.reflect.io.Path
@@ -11,6 +14,14 @@ class TestLong {
   def simpleLong() {
     val inputDirectory = Path("")
     TestUtils.compareWithJavacAfterRunning("SimpleLong", inputDirectory)
+  }
+
+  @Test
+  def longWithoutPoptimize() {
+    val inputDirectory = Path("")
+    val regularParticles = JavaCompiler.javaCompilerTransformations
+    val withoutPoptimize = regularParticles.filter(p => p != PoptimizeC)
+    new TestUtils(new CompilerFromParticles(withoutPoptimize)).compareWithJavacAfterRunning("SimpleLong", inputDirectory)
   }
 
 }
