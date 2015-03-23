a5643cc8f07444d6074720dd144e803e64caa8de
Remy Willems <rgv.willems@gmail.com>
2015-03-23 19:30:56 +0100

    DefaultConstructorC does not insertion of there already are constructors.

--- src/main/transformations/javac/constructor/DefaultConstructorC.scala
+++ src/main/transformations/javac/constructor/DefaultConstructorC.scala
@@ -11,8 +11,11 @@ object DefaultConstructorC extends ParticleWithPhase {
   def transform(clazz: Node, state: CompilationState): Unit = {
     val className = JavaClassSkeleton.getClassName(clazz)
     val members = JavaClassSkeleton.getMembers(clazz)
-    val constructor = ConstructorC.constructor(className, Seq(), Seq(), PublicVisibility)
-    clazz(JavaClassSkeleton.Members) = Seq(constructor) ++ members
+    val noConstructors = members.filter(member => member.clazz == ConstructorC.ConstructorKey).isEmpty
+    if (noConstructors) {
+      val defaultConstructor = ConstructorC.constructor(className, Seq(), Seq(), PublicVisibility)
+      clazz(JavaClassSkeleton.Members) = Seq(defaultConstructor) ++ members
+    }
   }
 
   override def description: String = "Adds a default public constructor to a class if none is specified."
