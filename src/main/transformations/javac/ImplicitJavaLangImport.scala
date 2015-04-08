456cd7db411a7e912590f13ca739b6d82e7eced5
Remy Willems <rgv.willems@gmail.com>
2015-04-08 19:11:31 +0200

    Removed all hardcoded signatures from JavaLang.

--- src/main/transformations/javac/ImplicitJavaLangImport.scala
+++ src/main/transformations/javac/ImplicitJavaLangImport.scala
@@ -7,14 +7,18 @@ import transformations.javac.classes.skeleton.JavaClassSkeleton
 import JavaClassSkeleton._
 
 object ImplicitJavaLangImport extends ParticleWithPhase {
+  val javaPackageName: String = "java"
+  val langPackageName: String = "lang"
+
   override def dependencies: Set[Contract] = Set(JavaClassSkeleton, WildcardImportC)
 
   override def transform(program: Node, state: CompilationState): Unit = {
     val clazz = program
     val imports = clazz.imports
-    val implicitImport = WildcardImportC.wildCardImport(Seq(JavaLang.javaPackageName, JavaLang.langPackageName))
+    val implicitImport = WildcardImportC.wildCardImport(Seq(javaPackageName, langPackageName))
     clazz.imports = Seq(implicitImport) ++ imports
   }
+
 
   override def description: String = "Implicitly adds an import to java.lang"
 }
--- src/main/transformations/javac/JavaLang.scala
+++ src/main/transformations/javac/JavaLang.scala
@@ -4,7 +4,7 @@ import core.particles.CompilerFromParticles
 import core.particles.node.Node
 import transformations.bytecode.readJar.ClassFileSignatureDecompiler
 import transformations.javac.classes.ClassCompiler
-import transformations.javac.classes.skeleton.{ClassSignature, MyCompiler, PackageSignature}
+import transformations.javac.classes.skeleton.{MyCompiler, PackageSignature}
 import util.TestUtils
 
 object JavaLang {
@@ -13,20 +13,14 @@ object JavaLang {
   val systemClass: Node = compiler.parseAndTransform(TestUtils.getTestFile("System.class").inputStream()).program
   val printStreamClass: Node = compiler.parseAndTransform(TestUtils.getTestFile("PrintStream.class").inputStream()).program
   val objectClass: Node = compiler.parseAndTransform(TestUtils.getTestFile("Object.class").inputStream()).program
-  //val stringClass: Node = compiler.parseAndTransform(TestUtils.getTestFile("String.class").inputStream()).program
+  val stringClass: Node = compiler.parseAndTransform(TestUtils.getTestFile("String.class").inputStream()).program
 
   def initialise(compiler: MyCompiler) {
     new ClassCompiler(objectClass, compiler)
-    //new ClassCompiler(stringClass, compiler)
+    new ClassCompiler(stringClass, compiler)
     new ClassCompiler(systemClass, compiler)
     new ClassCompiler(printStreamClass, compiler)
-    val javaLangPackage = compiler.find(Seq(javaPackageName, langPackageName)).asInstanceOf[PackageSignature]
-    val stringClassName = "String"
-    javaLangPackage.content.put(stringClassName, new ClassSignature(javaLangPackage, stringClassName))
   }
 
   val classPath = new PackageSignature(None, "")
-  val javaPackageName: String = "java"
-  val langPackageName: String = "lang"
-
 }
