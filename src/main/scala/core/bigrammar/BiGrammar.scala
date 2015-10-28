2eb5cebc3f33d2ac17ef2eda340770f158334262
Remy Willems <remy.willems@ortec-finance.com>
2015-10-28 09:20:07 +0100

    Extracted file

--- src/main/scala/core/bigrammar/BiGrammar.scala
+++ src/main/scala/core/bigrammar/BiGrammar.scala
@@ -5,33 +5,7 @@ import core.grammar.{Grammar, Identifier, NumberG, PrintGrammar, ~}
 import core.particles.node.Node
 import core.responsiveDocument.ResponsiveDocument
 
-trait GrammarDocumentWriter {
 
-  def identifier: BiGrammar = consume(Identifier)
-
-  def number: BiGrammar = consume(NumberG)
-
-  def integer = number ^^ ((s: Any) => Integer.parseInt(s.asInstanceOf[String]), (i: Any) => Some(i.toString))
-
-  def failure: BiGrammar = BiFailure
-
-  def produce(value: Any): BiGrammar = new Produce(value)
-
-  def space: BiGrammar = print(new WhiteSpace(1, 1))
-
-  def keyword(word: String): BiGrammar = new Keyword(word)
-
-  implicit def consume(grammar: Grammar): BiGrammar = new Consume(grammar)
-
-  implicit def print(document: ResponsiveDocument): BiGrammar = new Print(document)
-
-  implicit def print(document: Document): BiGrammar = new Print(document)
-
-  implicit def stringToGrammar(value: String): BiGrammar =
-    if (value.forall(c => Character.isLetterOrDigit(c)))
-      new Keyword(value)
-    else new Delimiter(value)
-}
 
 trait BiGrammar extends GrammarDocumentWriter {
 
---
+++ src/main/scala/core/bigrammar/GrammarDocumentWriter.scala
@@ -0,0 +1,29 @@
+package core.bigrammar
+
+trait GrammarDocumentWriter {
+
+  def identifier: BiGrammar = consume(Identifier)
+
+  def number: BiGrammar = consume(NumberG)
+
+  def integer = number ^^ ((s: Any) => Integer.parseInt(s.asInstanceOf[String]), (i: Any) => Some(i.toString))
+
+  def failure: BiGrammar = BiFailure
+
+  def produce(value: Any): BiGrammar = new Produce(value)
+
+  def space: BiGrammar = print(new WhiteSpace(1, 1))
+
+  def keyword(word: String): BiGrammar = new Keyword(word)
+
+  implicit def consume(grammar: Grammar): BiGrammar = new Consume(grammar)
+
+  implicit def print(document: ResponsiveDocument): BiGrammar = new Print(document)
+
+  implicit def print(document: Document): BiGrammar = new Print(document)
+
+  implicit def stringToGrammar(value: String): BiGrammar =
+    if (value.forall(c => Character.isLetterOrDigit(c)))
+      new Keyword(value)
+    else new Delimiter(value)
+}
