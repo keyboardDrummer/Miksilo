53320c72117300edc30e8daaf3740993b183017b
Remy Willems <remy.willems@ortec-finance.com>
2015-11-25 18:24:04 +0100

    Added todo

--- src/main/scala/core/bigrammar/printer/BiGrammarToPrinter.scala
+++ src/main/scala/core/bigrammar/printer/BiGrammarToPrinter.scala
@@ -122,7 +122,7 @@ class BiGrammarToPrinter {
 
   def extractProduct(value: Any, grammar: BiGrammar): Try[core.grammar.~[Any, Any]] = value match {
     case ~(left, right) => Try(core.grammar.~(left, right))
-    case VoidValue => Try(core.grammar.~(VoidValue, VoidValue))
+    case VoidValue => Try(core.grammar.~(VoidValue, VoidValue)) //TODO is this really necessary?
     case _ => fail("value was not a product.")
   }
 }
