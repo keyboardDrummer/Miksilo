453f1c51c9b03cea2c93abb53645c90dd4d3829d
Remy Willems <rgv.willems@gmail.com>
2016-11-06 13:20:36 +0100

    Small fix.

--- src/main/scala/core/bigrammar/DeepCloneBiGrammar.scala
+++ src/main/scala/core/bigrammar/DeepCloneBiGrammar.scala
@@ -1,6 +1,6 @@
 package core.bigrammar
 
-class DeepCloneBiGrammar extends BiGrammarObserver[BiGrammar] {
+class DeepCloneBiGrammar extends BiGrammarObserver[BiGrammar] { //TODO deze class gaat wel eens stuk, misschien omdat de default case soms teveel afvangt. Fixen?
 
   override def labelledEnter(name: AnyRef): BiGrammar = new Labelled(name)
 
@@ -13,6 +13,7 @@ class DeepCloneBiGrammar extends BiGrammarObserver[BiGrammar] {
     case mapGrammar: MapGrammar => new MapGrammar(helper(mapGrammar.inner), mapGrammar.construct, mapGrammar.deconstruct, mapGrammar.showMap)
     case sequence:Sequence => new Sequence(helper(sequence.first), helper(sequence.second))
     case topBottom: TopBottom => new TopBottom(helper(topBottom.first), helper(topBottom.second))
+    case as:As => As(helper(as.inner), as.key)
     case _ => self
   }
 }
