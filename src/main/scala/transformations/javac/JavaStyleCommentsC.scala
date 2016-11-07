a0feac5b1d285edb949200e521d48d89e21179a6
Remy Willems <remy.willems@ortec-finance.com>
2016-11-07 21:59:22 +0100

    Cleaned up Java style comments delta

--- src/main/scala/transformations/javac/JavaStyleCommentsC.scala
+++ src/main/scala/transformations/javac/JavaStyleCommentsC.scala
@@ -11,29 +11,6 @@ import scala.util.matching.Regex
 
 object JavaStyleCommentsC extends ParticleWithGrammar {
 
-  class GrammarAnalysis extends DataFlowAnalysis[GrammarPath,Boolean]
-  {
-    val biGrammarClass = classOf[BiGrammar]
-    override def getOutgoingNodes(path: GrammarPath): Set[GrammarPath] = {
-      path.children.toSet
-    }
-
-    override def updateState(state: Boolean, path: GrammarPath): Boolean = {
-      path.get match {
-        case _: NodeMap => true
-        case _: MapGrammar => false
-        case _ => state
-      }
-    }
-
-    override def combineState(first: Boolean, second: Boolean): Option[Boolean] = {
-      if (first == second)
-        return None
-      
-      Some(first && second)
-    }
-  }
-  
   case class CommentCollection(comments: Seq[String])
 
   object CommentKey extends Key
@@ -42,12 +19,8 @@ object JavaStyleCommentsC extends ParticleWithGrammar {
 
     val commentsGrammar = grammars.create(CommentGrammar, getCommentsGrammar.as(CommentKey))
 
-    val analysis = new GrammarAnalysis()
-    analysis.run(new RootGrammar(grammars.find(ProgramGrammar)), false)
-    
-    for(pathWithState <- analysis.states)
+    for(path <- new RootGrammar(grammars.find(ProgramGrammar)).descentsIncludingSelf)
     {
-      val path = pathWithState._1
       path match {
         case selection: GrammarSelection =>
           val current = selection.get
@@ -72,18 +45,22 @@ object JavaStyleCommentsC extends ParticleWithGrammar {
 
   def addCommentToNodeMap(commentsGrammar: BiGrammar, nodeMapPath: GrammarSelection): Unit = {
     val nodeMapToTransform = nodeMapPath.get.asInstanceOf[NodeMap]
-    val growers = nodeMapPath.ancestors.
-      map(path => path.get).
+    val verticalNotHorizontal: Boolean = getCommentVerticalOrHorizontal(nodeMapPath)
+
+    val innerWithComment = if (verticalNotHorizontal) commentsGrammar %> nodeMapToTransform.inner
+                           else commentsGrammar ~~> nodeMapToTransform.inner
+    nodeMapToTransform.inner = innerWithComment
+  }
+
+  def getCommentVerticalOrHorizontal(nodeMapPath: GrammarSelection): Boolean = {
+    val growers = nodeMapPath.ancestors.map(path => path.get).
       filter(grammar => grammar.isInstanceOf[TopBottom] || grammar.isInstanceOf[Sequence] || grammar.isInstanceOf[ManyVertical] || grammar.isInstanceOf[ManyHorizontal])
 
-    if (growers.isEmpty)
-      return
-
-    val firstGrower = growers.head
-    val verticalNotHorizontal = firstGrower.isInstanceOf[TopBottom] || firstGrower.isInstanceOf[ManyVertical]
-    val innerWithComment = if (verticalNotHorizontal) commentsGrammar %> nodeMapToTransform.inner
-                           else commentsGrammar ~> nodeMapToTransform.inner
-    nodeMapToTransform.inner = innerWithComment
+    val verticalNotHorizontal = growers.nonEmpty && {
+      val firstGrower = growers.head
+      firstGrower.isInstanceOf[TopBottom] || firstGrower.isInstanceOf[ManyVertical]
+    }
+    verticalNotHorizontal
   }
 
   def getCommentsGrammar: BiGrammar = {
