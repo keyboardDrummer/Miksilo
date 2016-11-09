1a75c9cb639341d4bc357aab1945e264d6a357c0
Remy Willems <remy.willems@ortec-finance.com>
2016-11-09 21:50:22 +0100

    Slight rename.

--- src/main/scala/application/compilerCockpit/MarkOutputGrammar.scala
+++ src/main/scala/application/compilerCockpit/MarkOutputGrammar.scala
@@ -3,5 +3,6 @@ package application.compilerCockpit
 import core.particles.Delta
 
 object MarkOutputGrammar extends Delta {
+  override def name = "pretty print"
   override def description: String = "Use as an anchor to specify where the output grammar is defined."
 }
