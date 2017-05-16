67b7d347e134c11adfdad55d166901f4615d30c9
Willems <rwillems@amazon.com>
2017-05-16 09:18:44 +0200

    Turned on ignored test

--- src/test/scala/core/bigrammar/TestColumnAlignment.scala
+++ src/test/scala/core/bigrammar/TestColumnAlignment.scala
@@ -1,13 +1,16 @@
 package core.bigrammar
 
 import org.scalatest.FunSuite
+import org.scalatest.exceptions.TestFailedException
 
 class TestColumnAlignment extends FunSuite with GrammarDocumentWriter {
 
-  ignore("test") {
+  //This test shows that when translating the bigrammars to parsers, some of the structure is lost.
+  //We might expect this parse and print to become identity for this input, but it doesn't even parse.
+  test("test") {
     val input = "Hallo.              Hier staat een tweede gesprek." + System.lineSeparator() +
                 "Hoe gaat het met je?"
     val grammar: BiGrammar = (("Hallo." : BiGrammar) % "Hoe gaat het met je?") ~ "Hier staat een tweede gesprek."
-    TestGrammarUtils.parseAndPrintSame(input, None, grammar)
+    assertThrows[TestFailedException](TestGrammarUtils.parseAndPrintSame(input, None, grammar))
   }
 }
