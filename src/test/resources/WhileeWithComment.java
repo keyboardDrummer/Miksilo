bcfc2742328d67e56aa3721a1ef459dcd04c4cc7
Willems <rgv.willems@gmail.com>
2017-11-10 09:37:48 +0100

    Made test a bit more interesting

--- src/test/resources/WhileeWithComment.java
+++ src/test/resources/WhileeWithComment.java
@@ -1,7 +1,7 @@
 class WhileeWithComment
 {
     /* SomeComment1 */
-    public static void main(String[] args)
+    public /* inner comment */ static void main(String[] args)
     {
         int i = 0;
         /* SomeComment2 */
