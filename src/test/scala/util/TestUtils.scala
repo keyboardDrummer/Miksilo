859c430c3a205c878813cf08baedf124bdde842a
Willems <rgv.willems@gmail.com>
2018-01-09 09:21:01 +0100

    Add name

--- src/test/scala/util/TestUtils.scala
+++ src/test/scala/util/TestUtils.scala
@@ -162,7 +162,7 @@ class TestUtils(val compiler: TestingLanguage) extends FunSuite {
     val path = Files.createTempDirectory("input")
     val fileName = name + ".java"
     Files.copy(inputStream, path.resolve(fileName))
-    Directory(output).createDirectory(true)
+    Directory(output).createDirectory(force = true)
     val processBuilder = Process.apply(s"javac -d $output $fileName", path.toFile)
     val logger = new LineProcessLogger()
     val exitValue = processBuilder ! logger
