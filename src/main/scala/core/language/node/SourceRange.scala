e6ca8db46e8c5fdebb6a95e387c322d1ed053916
Willems <rgv.willems@gmail.com>
2018-02-24 14:49:24 +0100

    Fix

--- src/main/scala/core/language/node/SourceRange.scala
+++ src/main/scala/core/language/node/SourceRange.scala
@@ -2,6 +2,6 @@ package core.language.node
 
 case class SourceRange(start: Position, end: Position) {
   def contains(position: Position): Boolean = {
-    position <= start && start <= end
+    start <= position && position <= end
   }
 }
