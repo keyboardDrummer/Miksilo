b87ed7ea4bad4f29421e42a1d055065bf5eaa54e
Willems <rgv.willems@gmail.com>
2018-10-14 21:33:44 +0200

    Small refactoring

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -1,15 +1,30 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
 'use strict';
 
 import { workspace, ExtensionContext } from 'vscode';
 import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
 
+interface LanguageConfiguration {
+	vscodeName: string,
+	miksiloName?: string,
+}
+
 export function activate(context: ExtensionContext) {
 	
+	const languages: Array<LanguageConfiguration> = [
+		{ 
+			vscodeName: "systemverilog", 
+			miksiloName: "verilog"
+		}]
+
+	for(const language of languages) {
+		activateLanguage(context, language)
+	}
+}
+
+function activateLanguage(context: ExtensionContext, language: LanguageConfiguration) {
+
 	const jar: string = workspace.getConfiguration('miksilo').get("jar") || process.env.MIKSILO;	
+	language.miksiloName = language.miksiloName || language.vscodeName;
 
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
@@ -18,25 +33,22 @@ export function activate(context: ExtensionContext) {
 		args: ["-jar",
 			//"-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=6007",
 			jar, 
-			"verilog"]
+			language.miksiloName]
 	}
 	
-	// Options to control the language client
 	let clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{scheme: 'file', language: 'systemverilog'}],
+		documentSelector: [{scheme: 'file', language: language.vscodeName}],
 		synchronize: {
-			// Synchronize the setting section 'languageServerExample' to the server
-			configurationSection: 'miskilo',
+			configurationSection: 'miksilo',
 			// Notify the server about file changes to '.clientrc files contain in the workspace
 			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
 		}
 	}
 	
-	// Create the language client and start the client.
-	let disposable = new LanguageClient('miskilo', 'System Verilog', serverOptions, clientOptions).start();
+	let disposable = new LanguageClient(
+		'miksilo' + language.vscodeName, 
+		language.vscodeName + " Miksilo", 
+		serverOptions, clientOptions).start();
 	
-	// Push the disposable to the context's subscriptions so that the 
-	// client can be deactivated on extension deactivation
 	context.subscriptions.push(disposable);
 }
