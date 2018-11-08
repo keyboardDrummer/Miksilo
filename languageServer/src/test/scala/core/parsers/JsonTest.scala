385c7fd78232a6e250db9aa8631bbe30e14be247
Willems <rgv.willems@gmail.com>
2018-11-08 20:21:22 +0100

    More tests

--- languageServer/src/test/scala/core/parsers/JsonTest.scala
+++ languageServer/src/test/scala/core/parsers/JsonTest.scala
@@ -9,10 +9,18 @@ class JsonTest extends FunSuite with CommonParsers {
   object UnknownExpression
   private lazy val jsonParser: Parser[Any] = WithDefault(stringLiteral | objectParser | wholeNumber, UnknownExpression)
 
-  test("basic") {
+  test("object with single member with number value") {
     val input = """{"person":3}"""
     val result = jsonParser.parse(StringReader(input.toCharArray))
-    assert(result.isInstanceOf[ParseSuccess[_]])
+    val value = getSuccessValue(result)
+    assertResult(List(("person","3")))(value)
+  }
+
+  test("object with single member with string value") {
+    val input = """{"person":"remy"}"""
+    val result = jsonParser.parse(StringReader(input.toCharArray))
+    val value = getSuccessValue(result)
+    assertResult(List(("person","remy")))(value)
   }
 
   test("garbage after number") {
@@ -50,7 +58,7 @@ class JsonTest extends FunSuite with CommonParsers {
     val parser = jsonParser
     val result = parser.parse(StringReader(input.toCharArray))
     val failure: ParseFailure[Any] = getFailure(result)
-    assertResult(Some(List(("""person""", UnknownExpression))))(failure.partialResult)
+    assertResult(Some(List(("person", UnknownExpression))))(failure.partialResult)
   }
 
   test("object member with only an unfinished key") {
@@ -58,11 +66,24 @@ class JsonTest extends FunSuite with CommonParsers {
     val parser = jsonParser
     val result = parser.parse(StringReader(input.toCharArray))
     val failure: ParseFailure[Any] = getFailure(result)
-    assertResult(Some(List(("""person""", UnknownExpression))))(failure.partialResult)
+    assertResult(Some(List(("person", UnknownExpression))))(failure.partialResult)
+  }
+
+  test("object member with an unfinished value") {
+    val input = """{"person":"remy"""
+    val parser = jsonParser
+    val result = parser.parse(StringReader(input.toCharArray))
+    val failure: ParseFailure[Any] = getFailure(result)
+    assertResult(Some(List(("person", "remy"))))(failure.partialResult)
   }
 
   private def getFailure(result: ParseResult[Any]): ParseFailure[Any] = {
     assert(result.isInstanceOf[ParseFailure[_]])
     result.asInstanceOf[ParseFailure[Any]]
+  }
+
+  private def getSuccessValue(result: ParseResult[Any]) = {
+    assert(result.isInstanceOf[ParseSuccess[_]])
+    result.asInstanceOf[ParseSuccess[Any]].result
   }
 }
