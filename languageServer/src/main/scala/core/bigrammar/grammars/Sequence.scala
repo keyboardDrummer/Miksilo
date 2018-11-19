f53505519a03bed5dac2349273178bcbf5f5ba62
Willems <rgv.willems@gmail.com>
2018-11-19 20:44:25 +0100

    Fixed a bug

--- languageServer/src/main/scala/core/bigrammar/grammars/Sequence.scala
+++ languageServer/src/main/scala/core/bigrammar/grammars/Sequence.scala
@@ -8,15 +8,17 @@ object Sequence {
   def identity: SequenceBijective = SequenceBijective(packTuple, unpackTuple)
 
   private def packTuple: (Any, Any) => (Any, Any) = (a: Any, b: Any) => (a,b)
-  private def unpackTuple: Any => (Any, Any) = {
-    case UndefinedDestructuringValue => (UndefinedDestructuringValue, UndefinedDestructuringValue)
-    case t: (Any, Any) => t
+  private def unpackTuple: Any => Option[(Any, Any)] = {
+    case UndefinedDestructuringValue => Some(UndefinedDestructuringValue, UndefinedDestructuringValue)
+    case t: (Any, Any) => Some(t)
+    case _ => None
   }
-  def ignoreLeft = SequenceBijective((a: Any, b: Any) => b, x => (UndefinedDestructuringValue, x))
-  def ignoreRight = SequenceBijective((a: Any, b: Any) => a, x => (x, UndefinedDestructuringValue))
+
+  def ignoreLeft = SequenceBijective((a: Any, b: Any) => b, x => Some(UndefinedDestructuringValue, x))
+  def ignoreRight = SequenceBijective((a: Any, b: Any) => a, x => Some(x, UndefinedDestructuringValue))
 }
 
-case class SequenceBijective(construct: (Any, Any) => Any, destruct: Any => (Any, Any))
+case class SequenceBijective(construct: (Any, Any) => Any, destruct: Any => Option[(Any, Any)])
 
 trait Sequence extends BiGrammar with Layout {
   def first: BiGrammar
