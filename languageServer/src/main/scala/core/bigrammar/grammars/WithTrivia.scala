9b21b376bcb409ff5cacb627c1c6eb738d0b30ff
Willems <rgv.willems@gmail.com>
2018-11-20 00:49:19 +0100

    Fixed a thing

--- languageServer/src/main/scala/core/bigrammar/grammars/WithTrivia.scala
+++ languageServer/src/main/scala/core/bigrammar/grammars/WithTrivia.scala
@@ -1,17 +1,29 @@
 package core.bigrammar.grammars
 
 import core.bigrammar.{BiGrammar, DefaultBiGrammarWriter}
+import core.language.node.GrammarKey
 
 object WithTrivia extends DefaultBiGrammarWriter {
   def withTrivia(grammar: BiGrammar, trivia: BiGrammar = ParseWhiteSpace, horizontal: Boolean = true): BiGrammar = {
-    if (horizontal) trivia ~> grammar
-    else trivia %> grammar
+    new WithTrivia(grammar, trivia, horizontal)
+//    if (horizontal) trivia ~> grammar
+//    else trivia %> grammar
   }
 
-  def getWithTrivia(grammar: BiGrammar, trivia: BiGrammar): Option[Sequence] = {
+  def getWithTrivia(grammar: BiGrammar): Option[WithTrivia] = {
     grammar match {
-      case sequence: Sequence if sequence.first == trivia => Some(sequence)
+      case sequence: WithTrivia => Some(sequence)
       case _ => None
     }
   }
+}
+
+object WithTriviaKey extends GrammarKey
+class WithTrivia(grammar: BiGrammar, trivia: BiGrammar = ParseWhiteSpace, val horizontal: Boolean = true)
+  extends Labelled(WithTriviaKey, if (horizontal) new LeftRight(trivia, grammar, Sequence.ignoreLeft) else new TopBottom(trivia, grammar, Sequence.ignoreLeft)) {
+  def getGrammar = grammar
+
+  override def containsParser(recursive: BiGrammar => Boolean): Boolean = true
+
+  override protected def getLeftChildren(recursive: BiGrammar => Seq[BiGrammar]) = recursive(grammar) //TODO maybe if we can remove all the WithTrivia's first in TriviaInsideNode we wouldn't need this hack.
 }
