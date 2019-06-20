3fe0cafef806dbca02b826aa0dbf692c91c1f1d7
Willems <rgv.willems@gmail.com>
2019-06-20 15:05:56 +0200

    Sped up test

--- languageServer/src/test/scala/deltas/smithy/SmithyTest.scala
+++ languageServer/src/test/scala/deltas/smithy/SmithyTest.scala
@@ -1,32 +1,27 @@
 package deltas.smithy
 
-import core.language.node.SourceRange
 import langserver.types
-import langserver.types.Location
 import languageServer.{HumanPosition, LanguageServerTest, MiksiloLanguageServer}
 import org.scalatest.FunSuite
 import util.SourceUtils
 
 class SmithyTest extends FunSuite with LanguageServerTest {
 
+  val server = new MiksiloLanguageServer(SmithyLanguage.language)
+  val program = SourceUtils.getTestFileContents("smithy/model.smithy")
+
   test("example model") {
-    val server = new MiksiloLanguageServer(SmithyLanguage.language)
-    val program = SourceUtils.getTestFileContents("smithy/model.smithy")
     val diagnostics = getDiagnostic(server, program)
     assert(diagnostics.size == 1)
   }
 
   test("goto definition") {
-    val server = new MiksiloLanguageServer(SmithyLanguage.language)
-    val program = SourceUtils.getTestFileContents("smithy/model.smithy")
     val definition = gotoDefinition(server, program, HumanPosition(6,22)).head.range
     val expectation = types.Range(HumanPosition(98, 11), HumanPosition(98, 25))
     assertResult(expectation)(definition)
   }
 
   test("goto definition $") {
-    val server = new MiksiloLanguageServer(SmithyLanguage.language)
-    val program = SourceUtils.getTestFileContents("smithy/model.smithy")
     val definition = gotoDefinition(server, program, HumanPosition(76, 35)).head.range
     val expectation = types.Range(HumanPosition(71, 3), HumanPosition(71, 12))
     assertResult(expectation)(definition)
