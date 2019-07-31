ae508c3002e707e4f7b0dfaee2c1815db0906201
Willems <rgv.willems@gmail.com>
2019-07-31 20:25:11 +0200

    Add ambiguity detection

---
+++ languageServer/src/main/scala/core/parsers/editorParsers/AmbiguityFindingParserWriter.scala
@@ -0,0 +1,111 @@
+package core.parsers.editorParsers
+
+trait AmbiguityFindingParserWriter extends CorrectingParserWriter {
+
+  override def findBestParseResult[Result](parser: Parser[Result], input: Input, mayStop: (Double, Double) => Boolean): SingleParseResult[Result] = {
+
+    val noResultFound = ReadyParseResult(None, input, History.error(FatalError(input, "Grammar is always recursive")))
+    var bestResult: ReadyParseResult[Result] = noResultFound
+
+    var resultsSeen = Map.empty[Any, ReadyParseResult[Result]]
+    var queue = parser(input, newParseState(input))
+    while(queue.nonEmpty) {
+      val (parseResult, tail) = queue.pop()
+
+      queue = parseResult match {
+        case parseResult: ReadyParseResult[Result] =>
+          val parseResultKey = ReadyParseResult(parseResult.resultOption, parseResult.remainder, getHistoryWithoutChoices(parseResult.history))
+          if (resultsSeen.contains(parseResultKey)) {
+            val previousResult = resultsSeen(parseResultKey)
+            val oldChoices = getHistoryChoices(previousResult.history)
+            val newChoices = getHistoryChoices(parseResult.history)
+            val mixedNoDrop = oldChoices.zip(newChoices)
+
+            // TODO equality check hier moet naar reference kijken.
+            val mixed = mixedNoDrop.
+              dropWhile(t => System.identityHashCode(t._1) == System.identityHashCode(t._2)).
+              takeWhile(t => System.identityHashCode(t._1) != System.identityHashCode(t._2)).reverse
+            throw new Exception("Your grammar produces duplicates" + previousResult)
+          }
+          else
+            resultsSeen += parseResultKey -> parseResult
+
+          bestResult = if (bestResult.score >= parseResult.score) bestResult else parseResult
+          tail match {
+            case tailCons: SRCons[Result] =>
+              if (mayStop(bestResult.originalScore, tailCons.head.score))
+                SREmpty
+              else
+                tail
+            case _ =>
+              SREmpty
+          }
+        case delayedResult: DelayedParseResult[Result] =>
+          val results = delayedResult.results
+          tail.merge(results)
+      }
+    }
+    SingleParseResult(bestResult.resultOption, bestResult.history.errors.toList)
+  }
+
+  def getHistoryChoices(history: History[Input]): Seq[(Input, Any)] = {
+    history match {
+      case withChoices: HistoryWithChoices[Input] => withChoices.choices
+      case _ => Seq.empty
+    }
+  }
+
+  def getHistoryWithoutChoices(history: History[Input]): History[Input] = {
+    history match {
+      case withChoices: HistoryWithChoices[Input] => withChoices.inner
+      case _ => history
+    }
+  }
+
+  override def choice[Result](first: Self[Result], other: => Self[Result], firstIsLonger: Boolean = false): Self[Result] =
+    if (firstIsLonger) new TrackingFirstIsLonger(first, other) else new TrackingChoice(first, other)
+
+  class TrackingFirstIsLonger[+First <: Result, +Second <: Result, Result](val first: Self[First], _second: => Self[Second])
+    extends ParserBuilderBase[Result] with ChoiceLike[Result] {
+
+    lazy val second = _second
+
+    override def getParser(recursive: GetParser): Parser[Result] = {
+      val parseFirst = recursive(first)
+      lazy val parseSecond = recursive(second)
+
+      new Parser[Result] {
+        override def apply(input: Input, state: ParseState) = {
+          val firstResult = parseFirst(input, state).addHistory(HistoryWithChoices(Seq(input -> first)))
+          val secondResult = parseSecond(input, state).addHistory(HistoryWithChoices(Seq(input -> second)))
+          firstResult match {
+            case cons: SRCons[Result]
+              if !cons.head.history.flawed => firstResult
+            case _ =>
+              firstResult.merge(secondResult)
+          }
+        }
+      }
+    }
+  }
+
+  class TrackingChoice[+First <: Result, +Second <: Result, Result](val first: Self[First], _second: => Self[Second])
+    extends ParserBuilderBase[Result] with ChoiceLike[Result] {
+
+    lazy val second = _second
+
+    override def getParser(recursive: GetParser): Parser[Result] = {
+      val parseFirst = recursive(first)
+      lazy val parseSecond = recursive(second)
+
+      new Parser[Result] {
+        override def apply(input: Input, state: ParseState) = {
+          val firstResult = parseFirst(input, state).addHistory(HistoryWithChoices(Seq(input -> first)))
+          val secondResult = parseSecond(input, state).addHistory(HistoryWithChoices(Seq(input -> second)))
+          val merged = firstResult.merge(secondResult)
+          merged
+        }
+      }
+    }
+  }
+}
