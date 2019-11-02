3b1c29509f17fe067654be2d78e752744f03bcc5
Willems <rwillems@amazon.com>
2019-11-02 14:24:55 +0100

    Change the stop function for CloudFormation languages to have a static time

--- modularLanguages/src/main/scala/deltas/cloudformation/CloudFormationLanguage.scala
+++ modularLanguages/src/main/scala/deltas/cloudformation/CloudFormationLanguage.scala
@@ -5,7 +5,7 @@ import java.io.InputStream
 import core.SolveConstraintsDelta
 import core.deltas._
 import core.language.Language
-import core.parsers.editorParsers.TimeRatioStopFunction
+import core.parsers.editorParsers.UntilTimeStopFunction
 import deltas.json.JsonLanguage
 import deltas.yaml.YamlLanguage
 
@@ -13,7 +13,7 @@ class CloudFormationLanguage(resourceSpecificationOption: Option[InputStream]) {
   val cloudFormationTemplate = new CloudFormationTemplate(resourceSpecificationOption)
   val jsonDeltas: Seq[Delta] = Seq(cloudFormationTemplate) ++
     JsonLanguage.deltas ++ Seq(SolveConstraintsDelta)
-  val jsonLanguage: Language = LanguageFromDeltas(Seq(ParseUsingTextualGrammar(TimeRatioStopFunction(10))) ++ jsonDeltas)
+  val jsonLanguage: Language = LanguageFromDeltas(Seq(ParseUsingTextualGrammar(UntilTimeStopFunction(200))) ++ jsonDeltas)
 
   val yamlDeltas: Seq[Delta] = Seq(YamlLanguage.parserDelta) ++
     Seq(ConvertObjectMemberKeysToStrings, ConvertTagsToObjectDelta, cloudFormationTemplate) ++
